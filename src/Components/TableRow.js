9b44a3f6e1ed4504e29028095c649257cca0062b
Utsab Roy <roy.utsab01@gmail.com>
2025-03-27 17:01:51 +0530

    changes_screen_270325__

--- src/Components/TableRow.js
+++ src/Components/TableRow.js
@@ -1,4 +1,4 @@
-import { EyeOutlined } from '@ant-design/icons';
+import { EditOutlined, EyeOutlined } from '@ant-design/icons';
 
 const TableRow = ({ index, data, navigate, curentPage }) => {
 
@@ -28,7 +28,7 @@ const TableRow = ({ index, data, navigate, curentPage }) => {
             className="text-blue-700 border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-1.5"
             onClick={() => navigate(`/home/admin_approval/AdApcrud/${data?.approval_no}`)}
           >
-            <EyeOutlined />
+            <EditOutlined />
           </button>
         </td>
       </tr>
