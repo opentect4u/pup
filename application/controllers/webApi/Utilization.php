2266f7c1dc40b221f9f1a3cf5283ed70de9c54ac
meettan <mondal.tanmoy@synergicsoftek.com>
2025-04-29 11:45:32 +0530

    chnage

--- application/controllers/webApi/Utilization.php
+++ application/controllers/webApi/Utilization.php
@@ -20,6 +20,7 @@ class Utilization extends CI_Controller {
             exit;
         }
         $this->validate_auth_key();
+		$this->load->helper('pdf');
     }
 
 	private function validate_auth_key() {
