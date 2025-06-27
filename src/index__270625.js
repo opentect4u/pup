38b578a5d3ed39414a6ab7363813247e46c0c7b6
unknown <roy.utsab01@gmail.com>
2025-06-27 13:02:49 +0530

    27/06/2025__ Loading chunk 7830 failed = FIXED

--- src/index__270625.js
+++ src/index__270625.js
@@ -2,84 +2,72 @@ import React, { lazy } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Democontext } from './Context/Democontext';
 import reportWebVitals from './reportWebVitals';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './Screens/HomeScreen/Home';
+import HomeScreen from './Screens/HomeScreen/HomeScreen';
+import FundRelListEditForm from './Screens/FUND_RELEASE/FundRelListEditForm';
+import FundExpListEditForm from './Screens/FUND_EXP/FundExpListEditForm';
+import UCListEditForm from './Screens/UC/UCListEditForm';
+import PRView from './Screens/Progress_Report/PRView';
+import PRDetails from './Screens/Progress_Report/PRDetails';
+import PRComp from './Screens/Progress_Report/PRComp';
+import MASTERComp from './Screens/MASTER/MASTERComp';
+import SECTOR_ADD_EDITForm from './Screens/MASTER/SECTOR_ADD_EDITForm';
+import { Democontext } from './Context/Democontext';
+import SOUR_OF_FUN_Form from './Screens/MASTER/SOUR_OF_FUN_Form';
+import IMPL_AGEN_Form from './Screens/MASTER/IMPL_AGEN_Form';
+import ACC_HEAD_Form from './Screens/MASTER/ACC_HEAD_Form';
+import REPORTComp from './Screens/REPORTS/REPORTComp';
+import Financial_Report from './Screens/REPORTS/Financial_Report';
+import HeadAccountwise_Report from './Screens/REPORTS/HeadAccountwise_Report';
+import Financial_Report_Graph from './Screens/REPORTS/Financial_Report_Graph';
+import HeadAccountwise_Report_Graph from './Screens/REPORTS/HeadAccountwise_Report_Graph';
+import USERComp from './Screens/USER_MANAGE/USERComp';
+import UserManage from './Screens/USER_MANAGE/UserManage';
+import ProfileComp from './Screens/PROFILE_MANAGE/ProfileComp';
+import ChangePass from './Screens/PROFILE_MANAGE/ChangePass';
+import Sector_Report from './Screens/REPORTS/Sector_Report';
+import Sector_Report_Graph from './Screens/REPORTS/Sector_Report_Graph';
+import District_Report from './Screens/REPORTS/District_Report';
+import District_Report_Graph from './Screens/REPORTS/District_Report_Graph';
+import Implement_Report from './Screens/REPORTS/Implement_Report';
+import Implement_Report_Graph from './Screens/REPORTS/Implement_Report_Graph';
 
+import DERTMENT_ADD from './Screens/MASTER/DERTMENT_ADD';
+import DESIGNATION_ADD from './Screens/MASTER/DESIGNATION_ADD';
+import UserProfile from './Screens/PROFILE_MANAGE/UserProfile';
+import Project_Submitted_By_Form from './Screens/MASTER/Project_Submitted_By_Form';
+import PCRView from './Screens/PCR_Generate/PCRView';
+import PCRComp from './Screens/PCR_Generate/PCRComp';
+import PCRForm from './Screens/PCR_Generate/PCRForm';
+import UC_Comp from './Screens/UC_Generate/UC_Comp';
+import UC_View from './Screens/UC_Generate/UC_View';
+import UC_Form from './Screens/UC_Generate/UC_Form';
+import Annex_Comp from './Screens/ANNEXURE/Annex_Comp';
+import Annex_View from './Screens/ANNEXURE/Annex_View';
+import Annex_Form from './Screens/ANNEXURE/Annex_Form';
+import ProjectStatusView from './Screens/Project_Status/ProjectStatusView';
+import ProjectStatusComp from './Screens/Project_Status/ProjectStatusComp';
+import ProjectStatusDetails from './Screens/Project_Status/ProjectStatusDetails';
 
-const loadable = (importFunc) => 
-  React.lazy(() => 
-    importFunc().catch((err) => {
-      console.error("Chunk load failed, reloading app...", err);
-      window.location.reload();  // Try fresh load
-      return new Promise(() => {});  // Prevent further errors
-    })
-  );
-
-const Home = loadable(() => import('./Screens/HomeScreen/Home'));
-const HomeScreen =loadable(()=>import('./Screens/HomeScreen/HomeScreen'));
-const FundRelListEditForm =loadable(()=>import('./Screens/FUND_RELEASE/FundRelListEditForm'));
-const FundExpListEditForm =loadable(()=>import('./Screens/FUND_EXP/FundExpListEditForm'));
-const UCListEditForm =loadable(()=>import('./Screens/UC/UCListEditForm'));
-const PRView =loadable(()=>import('./Screens/Progress_Report/PRView'));
-const PRDetails =loadable(()=>import('./Screens/Progress_Report/PRDetails'));
-const PRComp =loadable(()=>import('./Screens/Progress_Report/PRComp'));
-const MASTERComp =loadable(()=>import('./Screens/MASTER/MASTERComp'));
-const SECTOR_ADD_EDITForm =loadable(()=>import('./Screens/MASTER/SECTOR_ADD_EDITForm'));
-
-const SOUR_OF_FUN_Form =loadable(()=>import('./Screens/MASTER/SOUR_OF_FUN_Form'));
-const IMPL_AGEN_Form =loadable(()=>import('./Screens/MASTER/IMPL_AGEN_Form'));
-const ACC_HEAD_Form =loadable(()=>import('./Screens/MASTER/ACC_HEAD_Form'));
-const REPORTComp =loadable(()=>import('./Screens/REPORTS/REPORTComp'));
-const Financial_Report =loadable(()=>import('./Screens/REPORTS/Financial_Report'));
-const HeadAccountwise_Report =loadable(()=>import('./Screens/REPORTS/HeadAccountwise_Report'));
-const Financial_Report_Graph =loadable(()=>import('./Screens/REPORTS/Financial_Report_Graph'));
-const HeadAccountwise_Report_Graph =loadable(()=>import('./Screens/REPORTS/HeadAccountwise_Report_Graph'));
-const USERComp =loadable(()=>import('./Screens/USER_MANAGE/USERComp'));
-const UserManage =loadable(()=>import('./Screens/USER_MANAGE/UserManage'));
-const ProfileComp =loadable(()=>import('./Screens/PROFILE_MANAGE/ProfileComp'));
-const ChangePass =loadable(()=>import('./Screens/PROFILE_MANAGE/ChangePass'));
-const Sector_Report =loadable(()=>import('./Screens/REPORTS/Sector_Report'));
-const Sector_Report_Graph =loadable(()=>import('./Screens/REPORTS/Sector_Report_Graph'));
-const District_Report =loadable(()=>import('./Screens/REPORTS/District_Report'));
-const District_Report_Graph =loadable(()=>import('./Screens/REPORTS/District_Report_Graph'));
-const Implement_Report =loadable(()=>import('./Screens/REPORTS/Implement_Report'));
-const Implement_Report_Graph =loadable(()=>import('./Screens/REPORTS/Implement_Report_Graph'));
-
-const DERTMENT_ADD =loadable(()=>import('./Screens/MASTER/DERTMENT_ADD'));
-const DESIGNATION_ADD =loadable(()=>import('./Screens/MASTER/DESIGNATION_ADD'));
-const UserProfile =loadable(()=>import('./Screens/PROFILE_MANAGE/UserProfile'));
-const Project_Submitted_By_Form =loadable(()=>import('./Screens/MASTER/Project_Submitted_By_Form'));
-const PCRView =loadable(()=>import('./Screens/PCR_Generate/PCRView'));
-const PCRComp =loadable(()=>import('./Screens/PCR_Generate/PCRComp'));
-const PCRForm =loadable(()=>import('./Screens/PCR_Generate/PCRForm'));
-const UC_Comp =loadable(()=>import('./Screens/UC_Generate/UC_Comp'));
-const UC_View =loadable(()=>import('./Screens/UC_Generate/UC_View'));
-const UC_Form =loadable(()=>import('./Screens/UC_Generate/UC_Form'));
-const Annex_Comp =loadable(()=>import('./Screens/ANNEXURE/Annex_Comp'));
-const Annex_View =loadable(()=>import('./Screens/ANNEXURE/Annex_View'))
-const Annex_Form =loadable(()=>import('./Screens/ANNEXURE/Annex_Form'))
-const ProjectStatusView =loadable(()=>import('./Screens/Project_Status/ProjectStatusView'))
-const ProjectStatusComp =loadable(()=>import('./Screens/Project_Status/ProjectStatusComp'))
-const ProjectStatusDetails =loadable(()=>import('./Screens/Project_Status/ProjectStatusDetails'))
-
-const FundRelForm =loadable(()=>import('./Screens/FUND_RELEASE/FundRelForm'))
-const Sign_in =loadable(()=>import('./Screens/Auth/Sign_in'))
-const AdApForm =loadable(()=>import('./Screens/ADMIN_APPROVAL/AdApForm'))
-const TFForm =loadable(()=>import('./Screens/TENDER_FORMALITY/TFForm'))
-const FundExpForm =loadable(()=>import('./Screens/FUND_EXP/FundExpForm'))
-const UCForm =loadable(()=>import('./Screens/UC/UCForm'))
-const Auth = loadable(()=>import('./Screens/Auth/Auth'))
-const AdApComp = loadable(()=>import('./Screens/ADMIN_APPROVAL/AdApComp'))
-const AdApView = loadable(()=>import('./Screens/ADMIN_APPROVAL/AdApView'))
-const TFComp = loadable(()=>import('./Screens/TENDER_FORMALITY/TFComp'))
-const TFView = loadable(()=>import('./Screens/TENDER_FORMALITY/TFView'))
-const FundRelComp = loadable(()=>import('./Screens/FUND_RELEASE/FundRelComp'))
-const FundRelView = loadable(()=>import('./Screens/FUND_RELEASE/FundRelView'))
-const FundExpView = loadable(()=>import('./Screens/FUND_EXP/FundExpView'))
-const FundExpComp = loadable(()=>import('./Screens/FUND_EXP/FundExpComp'))
-const UCComp = loadable(()=>import('./Screens/UC/UCComp'))
-const UCView = loadable(()=>import('./Screens/UC/UCView'))
+const FundRelForm =lazy(()=>import('./Screens/FUND_RELEASE/FundRelForm'))
+const Sign_in =lazy(()=>import('./Screens/Auth/Sign_in'))
+const AdApForm =lazy(()=>import('./Screens/ADMIN_APPROVAL/AdApForm'))
+const TFForm =lazy(()=>import('./Screens/TENDER_FORMALITY/TFForm'))
+const FundExpForm =lazy(()=>import('./Screens/FUND_EXP/FundExpForm'))
+const UCForm =lazy(()=>import('./Screens/UC/UCForm'))
+const Auth = lazy(()=>import('./Screens/Auth/Auth'))
+const AdApComp = lazy(()=>import('./Screens/ADMIN_APPROVAL/AdApComp'))
+const AdApView = lazy(()=>import('./Screens/ADMIN_APPROVAL/AdApView'))
+const TFComp = lazy(()=>import('./Screens/TENDER_FORMALITY/TFComp'))
+const TFView = lazy(()=>import('./Screens/TENDER_FORMALITY/TFView'))
+const FundRelComp = lazy(()=>import('./Screens/FUND_RELEASE/FundRelComp'))
+const FundRelView = lazy(()=>import('./Screens/FUND_RELEASE/FundRelView'))
+const FundExpView = lazy(()=>import('./Screens/FUND_EXP/FundExpView'))
+const FundExpComp = lazy(()=>import('./Screens/FUND_EXP/FundExpComp'))
+const UCComp = lazy(()=>import('./Screens/UC/UCComp'))
+const UCView = lazy(()=>import('./Screens/UC/UCView'))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
